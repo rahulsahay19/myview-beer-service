cbd7ded2068f5176e5bf59597ef84c3f8bf54601
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-12 20:18:45 +0530

    Initial commit

---
+++ src/main/java/myview/springframework/myviewbeerservice/MyviewBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package myview.springframework.myviewbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyviewBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyviewBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myview/springframework/myviewbeerservice/MyviewBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package myview.springframework.myviewbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyviewBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
