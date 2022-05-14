64bc9a2558d67f363184db995591dd5c03409053
rahulsahay19 <rahulsahay19@hotmail.com>
2022-05-14 13:17:44 +0530

    More changes around jpa and data loader

---
+++ src/main/java/myview/springframework/myviewbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,63 @@
+package myview.springframework.myviewbeerservice.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import myview.springframework.myviewbeerservice.domain.Beer;
+import myview.springframework.myviewbeerservice.repositories.BeerRepository;
+import myview.springframework.myviewbeerservice.web.model.BeerStyleEnum;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+//This will run everytime spring context loads
+@RequiredArgsConstructor
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if(beerRepository.count() == 0 ) {
+            loadBeerObjects();
+        }
+    }
+
+    private void loadBeerObjects() {
+        Beer b1 = Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(Long.valueOf(BEER_1_UPC))
+                .build();
+
+        Beer b2 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(Long.valueOf(BEER_2_UPC))
+                .build();
+
+        Beer b3 = Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(Long.valueOf(BEER_3_UPC))
+                .build();
+
+        beerRepository.save(b1);
+        beerRepository.save(b2);
+        beerRepository.save(b3);
+    }
+}
---
+++ src/main/java/myview/springframework/myviewbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package myview.springframework.myviewbeerservice.repositories;
+
+import myview.springframework.myviewbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
